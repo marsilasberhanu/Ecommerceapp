91a7a9775131bebe045b0e513e3193777beb8d1e
marsilasberhanu <marsilasaddisu@gmail.com>
2026-05-25 15:47:59 +0300

    CheckoutPO

--- store/views.py
+++ store/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import *
 from django.http import JsonResponse
 import json
+import datetime
 
 # Create your views here.
 def store(request):
@@ -80,3 +81,34 @@ def updateItem(request):
     
 
     return JsonResponse('Item was added', safe=False)
+
+def processOrder(request):
+
+        transaction_id = datetime.datetime.now().timestamp()
+        data = json.loads(request.body)
+
+        if request.user.is_authenticated:
+            customer = request.user.customer
+            order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        else:
+            print ('User is not authenticated ...')
+
+        total = float(data['form']['total'])
+        order.transaction_id = transaction_id
+
+        if total == order.get_cart_total:
+            order.complete = True
+        order.save()
+        if order.shipping == True:
+            ShippingAddress.objects.create(
+            customer=customer,
+            order=order,
+            address=data['shipping']['address'],
+            city=data['shipping']['city'],
+            state=data['shipping']['state'],
+            zipcode=data['shipping']['zipcode'],
+            )
+    
+        return JsonResponse('Payment submitted ...', safe=False)
+
+
